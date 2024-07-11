69d3e269033946c7196cdec48f9cb8770d22a322
suhaib <muhammedsuhaibpottayil@gmail.com>
2024-07-11 19:58:00 +0530

    .

--- src/components/Chatbox.jsx
+++ src/components/Chatbox.jsx
@@ -3,9 +3,32 @@ import React from 'react'
 const Chatbox = () => {
   return (
     <>
-    <div className="w-full h-full flex items-center justify-center ">
+    {/* <div className="w-full h-full flex items-center justify-center ">
       <img src="/blackbr.png" alt="logo"  width={400}/>
-    </div>
+    </div> */}
+
+<div className="w-full bg-slate-500 ">
+
+  <div className="w-full">
+  <div  
+            className="w-full p-2 flex items-center justify-between hover:bg-gray-200 rounded-lg">
+              <div className="flex items-center">
+                <img
+                  src="https://i.ytimg.com/vi/ATElufr0OiE/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCJuaATtZxg3dXNAyUiTBM-dVbiOA"
+                  alt="profile Image"
+                  className="rounded-full h-12 w-12"
+                />
+                <p className="ml-3 font-bold">Suhaib</p>
+              </div>
+              <div className="flex items-center">
+                <p>03-03-2024</p>
+              </div>
+            </div>
+  </div>
+
+  <div className="w-full h-[50px] bg-red-900 fixed bottom-0"></div>
+</div>
+
 
 
     </>
